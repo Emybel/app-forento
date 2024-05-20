aa8ddc6f17b23ebe5606cafc0dc9f6e17b060bee
Imane BELAID <31738219+Emybel@users.noreply.github.com>
2024-05-20 11:01:24 +0100

    Set the archive_images and send_email_report functions

--- mongodb.py
+++ mongodb.py
@@ -1,10 +1,48 @@
-from pymongo import MongoClient
+# from pymongo import MongoClient
 
-client = MongoClient("localhost", 27017)
+# client = MongoClient("localhost", 27017)
 
-db = client.forento
+# db = client.forento
 
-detection = db.flyDetectionData
+# detection = db.flyDetectionData
 
-for data in detection.find():
-    print(data)
+# for data in detection.find():
+#     print(data)
+
+import os
+import re
+import shutil
+import zipfile
+from datetime import datetime
+
+def archive_images(storage="./data/storage", save_directory="./data/archive"):
+    # Create the save_directory if it doesn't exist
+    os.makedirs(save_directory, exist_ok=True)
+
+    # Group files by creation date
+    files_by_date = {}
+    for file in os.listdir(storage):
+        if file.endswith((".jpg", ".jpeg", ".png")):
+            file_path = os.path.join(storage, file)
+            file_name = os.path.basename(file_path)
+            print(f'File name : {file_name}')
+            date_str = file_name.split("_")[1]
+            print(f'date part:{date_str}')
+            try:
+                creation_date = datetime.strptime(date_str, "%m-%d-%y").date()
+                if creation_date not in files_by_date:
+                    files_by_date[creation_date] = []
+                files_by_date[creation_date].append(file_path)
+            except ValueError:
+                print(f"Skipping file {file_name} due to invalid date format")
+
+    # Create a zip file for each date
+    for date, files in files_by_date.items():
+        zip_filename = f"archive_{date.strftime('%Y%m%d')}.zip"
+        zip_path = os.path.join(save_directory, zip_filename)
+        with zipfile.ZipFile(zip_path, 'w', compression=zipfile.ZIP_DEFLATED) as archive:
+            for file_path in files:
+                archive.write(file_path)
+                print(f"Archiving {os.path.basename(file_path)} in {zip_filename}")
+
+archive_images()
--- util/sendolmail.py
+++ util/sendolmail.py
@@ -1,31 +1,80 @@
 import os
-import win32com.client as win32
+import re
+import smtplib
+import datetime
+from email import encoders
+from email.mime.text import MIMEText
+from email.mime.base import MIMEBase
+from email.mime.multipart import MIMEMultipart
 
-emails = {
-    'recipient_name': 'belaidimane@gmail.com',
-}
+archive_dir = "../data/archive"
 
-# Constract Outlook app instance
-olApp = win32.Dispatch('Outlook.Application')
-olNS = olApp.GetNameSpace('MAPI')
+def send_email_report(archive_dir):
+    """
+    Sends an email report with an attachment.
 
-#Construct the email item object
-mailItem = olApp.CreateItem(0)
-mailItem.Subject = "Daily Detection Report - [date]"
-mailItem.BodyFormat = 1
-mailItem.Body = f"""
-Hi,
+    Args:
+        archive_dir: The directory path where the zipped archives are stored.
+    """
 
-This is a daily email with an attached zip file containing detections made on [Date].
+    # Email configuration
+    sender_email = "imanebelaid@hotmail.com"
+    receiver_emails = ["belaidimane@gmail.com", "bakirdaoud@yotta.dz", "raniabenzaid@yotta.dz"]
+    subject = f"Daily Detection Report - {datetime.datetime.now().strftime('%m-%d-%Y')}"
 
-Please see the attached image for details.
+    # Find the zip file for the current date
+    current_date = datetime.datetime.now().date()
+    zip_pattern = r"archive_(\d{8})\.zip"
+    zip_filename = f"archive_{current_date.strftime('%Y%m%d')}.zip"
+    zip_path = os.path.join(archive_dir, zip_filename)
 
-Best regards,
-Forento
-"""
-mailItem.Attachments.Add(os.path.join(os.getcwd(), 'detected-fly_18-03-24_09-43-30.png'))
-mailItem.Display()
+    if os.path.exists(zip_path):
+        body_text = f"""Hi,
 
-for email in emails:
-    mailItem.To = emails[email]
+        This is a daily email with relevant information about detections made on {current_date.strftime('%m-%d-%Y')}.
+        Please see the attached zip archive for captured fly images.
 
+        Best regards,
+        Forento
+        """
+    else:
+        body_text = f"""Hi,
+
+        This is a daily email with relevant information about detections made on {current_date.strftime('%m-%d-%Y')}.
+        No flies were detected.
+
+        Best regards,
+        Forento
+        """
+
+    # Create message
+    msg = MIMEMultipart()
+    msg["From"] = sender_email
+    msg["To"] = ", ".join(receiver_emails)  # Join the list of recipients with commas
+    msg["Subject"] = subject
+    msg.attach(MIMEText(body_text, "plain"))
+
+    # Attach the zip file if it exists
+    if os.path.exists(zip_path):
+        with open(zip_path, "rb") as attachment:
+            part = MIMEBase("application", "octet-stream")
+            part.set_payload(attachment.read())
+        encoders.encode_base64(part)
+        part.add_header(
+            "Content-Disposition",
+            f"attachment; filename={os.path.basename(zip_path)}",
+        )
+        msg.attach(part)
+
+    app_pwd = "ipwtekmnicehktdm"
+    server = smtplib.SMTP(host='smtp-mail.outlook.com', port=587)
+    server.starttls()
+    server.login(sender_email, app_pwd)
+
+    status_code, response = server.ehlo()
+    print(f"Echoing server : {status_code} {response}")
+
+    server.send_message(msg, from_addr=sender_email, to_addrs=receiver_emails)
+
+# Call the function with the archive directory path
+send_email_report("../data/archive")
