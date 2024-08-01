9896bfa69248bf7e5139df0d08f190f1dacf621d
Imane BELAID <31738219+Emybel@users.noreply.github.com>
2024-08-01 12:05:22 +0100

    Clean up code.

--- login.py
+++ login.py
@@ -20,13 +20,7 @@ def login_user(email, password):
         return None, None
 
     email = email.strip().lower()
-    # print(f"email: {email}")
-    # print(f"password: {password}")
-    
     users = db.users.find()  # This returns a cursor to all documents in the collection
-    # for user in users:
-    #     print(user)
-    
     user = db.users.find_one({"email": email})
     # print(f"User credintials: {user}")
     
@@ -49,46 +43,3 @@ def login_user(email, password):
     else:
         CTkMessagebox(title="User Not Found", message="The provided email and username combination does not exist.")
         return None, None, None
-
-# if __name__ == "__main__":
-#     ctk.set_appearance_mode("dark")
-#     ctk.set_default_color_theme("green")
-
-#     login_window = ctk.CTk()
-#     login_window.title("Forento Fly Detector")
-#     login_window.geometry("480x900")
-
-#     header_frame = ctk.CTkFrame(login_window, corner_radius=5)
-#     header_frame.pack(side="top", fill="x", padx=10)
-
-#     logo_image = ctk.CTkImage(Image.open("asset/logo.png"), size=(80, 80))
-
-#     app_name_label = ctk.CTkLabel(header_frame, text="FORENTO Fly Detector", font=("Arial", 20), anchor="center")
-#     logo_label = ctk.CTkLabel(header_frame, image=logo_image, text=" ", anchor='center')
-#     logo_label.pack(side="left", padx=40, pady=5)
-#     app_name_label.pack(side="left", pady=5)
-
-#     login_frame = ctk.CTkFrame(login_window, corner_radius=5)
-#     login_frame.pack(padx=10, pady=10, fill="both", expand=True)
-
-#     auth_frame = ctk.CTkFrame(login_frame, border_width=1, border_color="#101c12", corner_radius=14)
-#     auth_frame.pack(padx=20, pady=20, fill="both", expand=True)
-
-#     label = ctk.CTkLabel(master=auth_frame, text="Login", font=("Roboto", 24))
-#     label.pack(pady=20, padx=10)
-
-#     email_entry = ctk.CTkEntry(master=auth_frame, placeholder_text="example@outlook.com")
-#     email_entry.pack(pady=12, padx=10)
-
-#     pwd_entry = ctk.CTkEntry(master=auth_frame, placeholder_text="Password", show="*")
-#     pwd_entry.pack(pady=12, padx=10)
-
-#     login_btn = ctk.CTkButton(
-#         master=auth_frame,
-#         text="Login",
-#         corner_radius=7,
-#         command=login_user
-#     )
-#     login_btn.pack(pady=15)
-
-#     login_window.mainloop()
